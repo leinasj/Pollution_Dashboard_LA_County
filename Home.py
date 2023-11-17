7a533a66912a893fede2a5ec24f8f3b71a0d1a59
James Leinas <jleinas24@gmail.com>
2023-11-17 14:27:19 -0800

    11/17/2023 Updates: Updates to Highest AQI Days table, Pie chart update for distribution of AQI Classifications

--- Home.py
+++ Home.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 import streamlit.components.v1 as components
 import mysql.connector
 import os
@@ -11,9 +13,10 @@ from Visualizations.plot_time_series import plot_ts
 from dotenv import load_dotenv
 load_dotenv()
 
+hide_streamlit_style = """ <style> footer {visibility: hidden;} </style> """
 st.set_page_config(page_title="Los Angeles County Air Pollutants 2000-Present", layout="wide", menu_items={"About":"Made by Leinas"}, initial_sidebar_state="collapsed")
 st.markdown("<h1 style='text-align: center;'>Los Angeles County Air Pollutants Dashboard</h1>", unsafe_allow_html=True)
-
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 with open('SQL\Query_Daily_Observations.sql', 'r') as s:
     sql_query = s.read()
@@ -40,6 +43,7 @@ source_code = HtmlFile.read()
 
 
 st.subheader(f"**Los Angeles County Live Observations**")
+st.caption(f"**Last Observation made at {live_data['HourObserved'].unique()[0]}:00 {live_data['LocalTimeZone'].unique()[0]}, {live_data['Date'].unique()[0].isoformat()}**")
 components.html(source_code, width=1000, height=400)
 
 with open('SQL\Query_weekly_observations.sql', 'r') as s:
@@ -48,13 +52,20 @@ with open('SQL\Query_weekly_observations.sql', 'r') as s:
 cursor.execute(sql_query)
 
 data = pd.DataFrame(cursor.fetchall(), columns=columns)
-# data = data.loc[data['HourObserved'] == max(data['HourObserved'])]
 # Calculate mid-point for annotation sliding based on date range
 mid_point = min(data['Date']) + (max(data['Date']) - min(data['Date']))/2
 
 st.divider()
+
+#****** 7-Day AQI Monitoring and AQI Forecast ******
+
+cities = np.append(data['City'].unique(), "Select All")
 col1, col2 = st.columns(2)
 with col1:
+    # Add selectbox for city if multiple are extracted from DB
+    city1 = st.selectbox(label = "**City**", options = np.roll(cities,1))
+    if city1 != 'Select All':
+        data = data.loc[data['City']==city1]
     st.plotly_chart(plot_ts(mid_point, data, "7 Day Air Quality Index (AQI) Monitoring", chart_type='scatter'), use_container_width=True, height= 200)
 
 columns = ["Date", "City", "Pollutant", "AQI", "AQI_Classification"]
@@ -65,12 +76,13 @@ with open('SQL\Query_Forecast.sql', 'r') as s:
 cursor.execute(sql_query)
 data = pd.DataFrame(cursor.fetchall(), columns = columns)
 with col2:
-    st.subheader("Tomorrow's AQI Forecast")
-    if data.empty:
-        st.write("Forecast Unavailable at the moment, come back later :)")
-    else:
-        
-        st.dataframe(data.sort_values(by = 'City'), use_container_width=True, hide_index=True)
+    
+    if data.loc[data['Date'] == datetime.date.today() + timedelta(days=1)].empty:
+        st.subheader("Todays's AQI Forecast")
+        st.dataframe(data.loc[data['Date'] == datetime.date.today()].sort_values(by = 'City'), use_container_width=True, hide_index=True)
+    elif not data.loc[data['Date'] == datetime.date.today() + timedelta(days=1)].empty:
+        st.subheader("Tomorrow's AQI Forecast")    
+        st.dataframe(data.loc[data['Date'] == datetime.date.today()+ timedelta(days=1)].sort_values(by = 'City'), use_container_width=True, hide_index=True)
     
 columns = ["Date", "City", "AQI", "AQI_Classification", "Month", "Action Days"]
 
@@ -82,6 +94,8 @@ x = cursor.fetchall()
 x = pd.DataFrame(x, columns=columns)
 
 st.divider()
+
+#****** Select Boxes for Filtering ******
     
 col3, col4, col5= st.columns(3)
 
@@ -95,31 +109,33 @@ with col5:
 
 # Filter data based on start and end dates selected by user 
 mask = (x['Date'] >=start) & (x['Date'] <= end)
-x = x.loc[(mask)]
-
-
-col7, col8 = st.columns([1, 1])
-x = x.loc[(x['City']==city)]
-with col7:
-    st.subheader("Highest AQI Days")
-    # x.groupby('City').get_group(city).agg({'max'})
-    # x2 = x2.loc[(x2['Date'].isin(x2.groupby('City').agg('max')['Date']))]
-    # st.dataframe(x.sort_values(by="City"), hide_index=True, use_container_width=True)
-    
-
-
-
-fig = px.pie(x, values='AQI_Classification', names='Month', title='Distribution of AQI Classes')
-st.plotly_chart(fig, use_container_width=True)
+x = x.loc[(mask) & (x['City']==city)]
+x['Action Days'] = x['Action Days'].replace({0:"No", 1:"Yes"})
+#****** Highest AQI Days and Time Series in Date Range ******
 # Add count of action days that occur in date range
-action_days = x.loc[(x['Action Days']==1) & (x['AQI_Classification']!="Unhealthy for Sensitive Groups")]
+action_days = x.loc[(x['Action Days']=="Yes")]
 
 # Calculate mid-point for annotation sliding based on date range
-mid_point = start + (end - start)/2
+mid_point = min(x['Date']) + (end - min(x['Date']))/2
 
+st.plotly_chart(plot_ts(mid_point, x, title = f"{city} AQI Time Series ({min(x['Date'])})-({max(x['Date'])})", chart_type='line'), use_container_width=True)
+st.write(f"Number of Action Days: <b>{len(action_days)}</b> <br>(Days that AQI falls in Unhealthy for Sensitive Groups, Unhealthy, Very Unhealthy or Hazardous Classification)", unsafe_allow_html=True)
+col7, col8 = st.columns(2)
+with col7:
+    st.caption(f"Highest AQI Days per Month in {city}")
+    idx = x.groupby('Month')['AQI'].idxmax()
+    max_AQI = x.loc[idx]
+    st.dataframe(max_AQI.sort_values(by="Month")[["Date", "AQI", "AQI_Classification", "Month", "Action Days"]], hide_index=True, use_container_width=True, height=450)
 
+color_map = ['green','yellow','orange','red','purple', 'maroon']
 
 with col8:
-    st.plotly_chart(plot_ts(mid_point, x, title = f"{city} AQI Time Series ({min(x['Date'])})-({max(x['Date'])})", chart_type='line'), use_container_width=True)
-    st.write(f"Number of Action Days: <b>{len(action_days)}</b> <br>(Days that AQI falls in Unhealthy, Very Unhealthy or Hazardous Classification)", unsafe_allow_html=True)
+    fig = go.Figure(data=[go.Pie(labels=x['AQI_Classification'].sort_values().unique(), values=x['AQI_Classification'].value_counts(normalize=True), hole=.3)])
+    fig.update_layout(title='Distribution of AQI Classes')
+    fig.update_traces(marker=dict(colors=color_map),hoverinfo='label+percent', textinfo='percent',textposition='inside')
+    st.plotly_chart(fig, use_container_width=True)
 
+
+
+cursor.close()
+cnx.close()
--- SQL/Query_Forecast.sql
+++ SQL/Query_Forecast.sql
@@ -1,2 +1,2 @@
 SELECT DISTINCT DateForecast as 'Date', City, ParameterName, AQI, AQI_Classification FROM air_pollution.forecast_data
-WHERE DateForecast = (curdate()+1) AND ParameterName = 'O3';
+WHERE DateForecast BETWEEN curdate() AND (curdate()+1) AND ParameterName = 'O3';
